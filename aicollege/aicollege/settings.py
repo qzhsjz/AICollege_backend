30c3cde7331cc9a459e1bd096dced07576a9858e
Angelo Qin <qzhsjz@gmail.com>
2018-05-25 11:18:53 +0800

    Database & Language Config

--- aicollege/aicollege/settings.py
+++ aicollege/aicollege/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'aicollege.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AICollege',
+        'USER': 'AICollege',
+        'PASSWORD': 'AICollege337845818',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 TIME_ZONE = 'UTC'
 
