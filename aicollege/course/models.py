cb44f8835a2fd9d2d3d35f8fb6c5b1470cf93cea
osierboy <2441531688@qq.com>
2018-05-30 22:29:11 +0800

    Database update

--- aicollege/course/models.py
+++ aicollege/course/models.py
@@ -1,3 +1,13 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+from aicollege.user.models import Student,Teacher
+
+class Course(models.Model):
+    #course_id = models.AutoField()
+    course_name = models.CharField(max_length=255)
+    credit = models.IntegerField(default=4)          #默认4学分
+    course_hours = models.IntegerField(default=48)   #默认48学时
+    course_info = models.textField()
+    teacher = models.ForeignKey(Teacher,on_delete=models.CASCADE) #老师与课程的关系为1对多
+    students = models.ManyToManyField(Student) #学生与课程的关系是多对多
