c9667ac66276ddaed41e726c03349886367476df
Angelo Qin <qzhsjz@gmail.com>
2018-06-16 14:26:38 +0800

    完善接口

--- aicollege/index/views.py
+++ aicollege/index/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def mainpage(request):
+    return HttpResponse("This is the AICollege API.")
