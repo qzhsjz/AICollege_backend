9306091e8b50db0c5227f9ffe5a4fc248d1f8729
Angelo Qin <qzhsjz@gmail.com>
2018-06-16 16:19:10 +0800

    fill url

--- aicollege/user/urls.py
+++ aicollege/user/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/<int:id>', admin.site.urls),
-    path('login/', views.login),
-    path('register/', views.regist),
-    path('emailverify/', views.email_verify),
-    path('chkemail/', views.check_email),
-    path('chkusername/', views.check_username),
-    path('chkid/', views.check_id),
-    path('picupload/', views.input_pic)
+    # path('admin/<int:id>', admin.site.urls),
+    path('login', views.login),
+    path('register', views.regist),
+    path('emailverify', views.email_verify),
+    path('chkemail', views.check_email),
+    path('chkusername', views.check_username),
+    path('chkid', views.check_id),
+    path('picupload', views.input_pic)
 ]
