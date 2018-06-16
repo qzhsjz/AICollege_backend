90aba5b4df15e2682fb283ef637bafe45b48834e
Angelo Qin <qzhsjz@gmail.com>
2018-06-16 14:23:10 +0800

    urls fill

--- aicollege/aicollege/urls.py
+++ aicollege/aicollege/urls.py
@@ -19,4 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('course/', include('course.urls')),
+    path('user/', include('user.urls')),
+    path('forum/', include('forum.urls')),
+    path('/', include('index.urls'))
 ]
