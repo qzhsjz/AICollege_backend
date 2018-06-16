4e078eedde2d7e73a560e2e9006034673c28d8ef
Angelo Qin <qzhsjz@gmail.com>
2018-06-16 16:14:22 +0800

    fill url

--- aicollege/user/urls.py
+++ aicollege/user/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('admin/<int:id>', admin.site.urls),
-    path('login/', views.login)
+    path('login/', views.login),
+    path('register/', views.regist),
+    path('emailverify/', views.email_verify),
+    path('chkemail/', views.check_email),
+    path('chkusername/', views.check_username),
+    path('chkid/', views.check_id),
+    path('picupload/', views.input_pic)
 ]
