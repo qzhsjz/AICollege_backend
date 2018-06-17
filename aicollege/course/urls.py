71c77f8aa813b9189352df8babb2fb7f766a0266
osierboy <2441531688@qq.com>
2018-06-17 18:18:06 +0800

    修改course

--- aicollege/course/urls.py
+++ aicollege/course/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     #ex: /course/
-    path('all/<int:index>',views.getCourseInfo),
+    path('all',views.getCourseInfo),
     #ex: /course/user_id/
-    path('<int:user_id>/', views.getCourseInfoUid),
-    path('<int:uid>/<int:cid>',views.judgeCourse),
-    path('<int:user_id>/<int:course_id>', views.getSectionInfoUCid),
+    path('', views.getCourseInfoUid),
+    path('<int:cid>',views.judgeCourse),
+    path('<int:course_id>', views.getSectionInfoUCid),
 ]
