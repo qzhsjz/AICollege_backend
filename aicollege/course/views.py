70de6f58bba7e7128a3433f78f7e74543dbda5c8
osierboy <2441531688@qq.com>
2018-06-18 13:52:02 +0800

    修改course

--- aicollege/course/views.py
+++ aicollege/course/views.py
@@ -60,6 +60,7 @@ def searchSection(uid,cid):
 
 #返回初始界面课程的信息,类似index，加入界面时返回申请
 def getCourseInfo(request,page):
+    print(request.session.keys())
     try:
         data = searchCourse(-1,int(page))  #-1用于表示非用户id，将所有课程推送回来
     except Course.DoesNotExist:  ##Course 表查找失败
@@ -68,6 +69,7 @@ def getCourseInfo(request,page):
 
 #返回用户购买课程列表，根据uid（所有课程的表）
 def getCourseInfoUid(request,page):
+    print(request.session.keys())
     user_id = request.session['uid']
     try:
         data = searchCourse(int(user_id),int(page))  #获取学生id对应的所有课程的所有信息
@@ -79,6 +81,7 @@ def getCourseInfoUid(request,page):
 
 #返回用户购买课程对应小节的列表，根据uid,cid（对应课程的所有小节表）（暂时没有考虑免费课程获取小节的情况）
 def getSectionInfoUCid(request,course_id):
+    print(request.session.keys())
     user_id = request.session['uid']
     try:
         data = searchSection(int(user_id),course_id)  #获取学生id对应的所有课程的所有信息
@@ -87,6 +90,7 @@ def getSectionInfoUCid(request,course_id):
     return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
 
 def judgeCourse(request,cid):
+    print(request.session.keys())
     uid = request.session['uid']
     #uid=10
     user = User.objects.filter(id= int(uid))[0]
