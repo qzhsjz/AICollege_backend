3e83093f00b76e0de9d65a6fd0b751807a9169a1
osierboy <2441531688@qq.com>
2018-06-18 17:27:26 +0800

    修改course

--- aicollege/course/views.py
+++ aicollege/course/views.py
@@ -1,36 +1,38 @@
 # Create your views here.
-from django.http import Http404,JsonResponse
-from .models import Course,Section
+from django.http import Http404, JsonResponse
+from .models import Course, Section
 from user.models import User
 from django.forms.models import model_to_dict
 
-##进入初始界面的时候，根据用户ID和推送算法，加载课程信息
-#根据需求返回课程信息
 
-#搜索算法搜索课程
-def searchCourse(uid,page):
+##进入初始界面的时候，根据用户ID和推送算法，加载课程信息
+# 根据需求返回课程信息
+
+# 搜索算法搜索课程
+def searchCourse(uid, page):
     if uid == -1:
-        course = Course.objects.all()  #选取所有的课程
+        course = Course.objects.all()  # 选取所有的课程
     else:
-        user = User.objects.filter(id = uid)[0]
-        course = Course.objects.filter(user__id = user.id).select_related()  #选取uid的所有课程
+        user = User.objects.filter(id=uid)[0]
+        course = Course.objects.filter(user__id=user.id).select_related()  # 选取uid的所有课程
 
     course1 = []
     obj_dic = {}
-    obj_dic['length'] = (len(course)-0.5)// 9+1
+    obj_dic['length'] = (len(course) - 0.5) // 9 + 1
     for o in course:
         # 把Object对象转换成Dict
         course1.append(model_to_dict(o))
     len1 = max(9, len(course1))
 
-    obj_dic['data'] = course1[(page-1)*9:min((page-1)*9+8,len1-1)]
+    obj_dic['data'] = course1[(page - 1) * 9:min((page - 1) * 9 + 8, len1 - 1)]
     return obj_dic
 
-#搜索小节
-def searchSection(uid,cid):
-    user = User.objects.filter(id = uid)[0]
+
+# 搜索小节
+def searchSection(uid, cid):
+    user = User.objects.filter(id=uid)[0]
     course = Course.objects.filter(id=cid, user__id=user.id).select_related()[0]  # 选取uid的所有课程
-    section = Section.objects.filter(course__id = course.id).select_related()
+    section = Section.objects.filter(course__id=course.id).select_related()
 
     section1 = []
     obj_dic = {}
@@ -39,64 +41,67 @@ def searchSection(uid,cid):
         # 把Object对象转换成Dict
         section1.append(model_to_dict(o))
 
-    #len1 = max(9,len(section1))
+    # len1 = max(9,len(section1))
     obj_dic['data'] = section1
     return obj_dic
 
-    #for o in section:
-        # 把Object对象转换成Dict
-        #dict = {}
-        #dict.update(o.__dict__)
-        #dict.pop("_state", None)  # 去除掉多余的字段
-        #obj_dic[o.id] = dict
-        #obj_dic
+    # for o in section:
+    # 把Object对象转换成Dict
+    # dict = {}
+    # dict.update(o.__dict__)
+    # dict.pop("_state", None)  # 去除掉多余的字段
+    # obj_dic[o.id] = dict
+    # obj_dic
 
-    #return obj_dic
-    #return model_to_dict(section)
-    #columns = [col[0] for col in section.description]
-    #return [
+    # return obj_dic
+    # return model_to_dict(section)
+    # columns = [col[0] for col in section.description]
+    # return [
     #    dict(zip(columns, row)) for row in section.fetchall()
-    #]
+    # ]
 
-#返回初始界面课程的信息,类似index，加入界面时返回申请
-def getCourseInfo(request,page):
+
+# 返回初始界面课程的信息,类似index，加入界面时返回申请
+def getCourseInfo(request, page):
     print(request.COOKIES)
     try:
-        data = searchCourse(-1,int(page))  #-1用于表示非用户id，将所有课程推送回来
-    except Course.DoesNotExist:  ##Course 表查找失败
-        raise Http404("课程加载失败")
-    return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
-
-#返回用户购买课程列表，根据uid（所有课程的表）
-def getCourseInfoUid(request,page):
-    print(request.COOKIES)
-    user_id = request.session['uid']
-    try:
-        data = searchCourse(int(user_id),int(page))  #获取学生id对应的所有课程的所有信息
-
+        data = searchCourse(-1, int(page))  # -1用于表示非用户id，将所有课程推送回来
     except Course.DoesNotExist:  ##Course 表查找失败
         raise Http404("课程加载失败")
     return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
-#返回用户购买课程对应小节的列表，根据uid,cid（对应课程的所有小节表）（暂时没有考虑免费课程获取小节的情况）
-def getSectionInfoUCid(request,course_id):
+# 返回用户购买课程列表，根据uid（所有课程的表）
+def getCourseInfoUid(request, page):
     print(request.COOKIES)
     user_id = request.session['uid']
     try:
-        data = searchSection(int(user_id),course_id)  #获取学生id对应的所有课程的所有信息
+        data = searchCourse(int(user_id), int(page))  # 获取学生id对应的所有课程的所有信息
+
+    except Course.DoesNotExist:  ##Course 表查找失败
+        raise Http404("课程加载失败")
+    return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+# 返回用户购买课程对应小节的列表，根据uid,cid（对应课程的所有小节表）（暂时没有考虑免费课程获取小节的情况）
+def getSectionInfoUCid(request, course_id):
+    print(request.COOKIES)
+    user_id = request.session['uid']
+    try:
+        data = searchSection(int(user_id), course_id)  # 获取学生id对应的所有课程的所有信息
     except Section.DoesNotExist:  ##Course 表查找失败
         raise Http404("课程小节加载失败")
     return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
 
-def judgeCourse(request,cid):
+
+def judgeCourse(request, cid):
     print(request.COOKIES)
     f = False
     try:
         uid = request.session['uid']
-        user = User.objects.filter(id= int(uid))[0]
+        user = User.objects.filter(id=int(uid))[0]
         course = Course.objects.filter(id=cid, user__id=user.id).select_related()  # 选取uid的所有课程
-        f= True
+        f = True
     except KeyError:
         f = False
 
@@ -124,9 +129,10 @@ def judgeCourse(request,cid):
 
     return JsonResponse(dict, safe=False, json_dumps_params={'ensure_ascii': False})
 
-def addCourse(request,cid):
+
+def addCourse(request, cid):
     uid = request.session['uid']
     user = User.objects.get(id=int(uid))
     course = Course.objects.get(id=int(cid))
-    course.entry_set.add(user)
-    course.save()
+    course.user.add(user)
+    #course.save()
