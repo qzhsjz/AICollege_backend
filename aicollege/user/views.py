8eca01aedb81dc3051e2479b4e3252386f90a834
Angelo Qin <qzhsjz@gmail.com>
2018-06-18 11:51:14 +0800

    Email Debug

--- aicollege/user/views.py
+++ aicollege/user/views.py
@@ -114,8 +114,9 @@ def regist(request):
 
 
         hostname = '39.106.19.27:8080'
-        mailbody = "欢迎注册小智课堂！请点击以下链接注册：http://" + hostname + "/user/emailverify?code=" + code + '&username=' + username
-        a = send_mail(subject='注册确认',message=mailbody,from_email='aicollege@126.com',recipient_list=[email])
+        verifyurl = "http://" + hostname + "/user/emailverify?code=" + code + '&username=' + username
+        mailbody = "欢迎注册小智课堂！请<a href=" + verifyurl + " target=_blank>点击这里</a>验证邮箱，或手动复制以下链接链接注册：<br>" + verifyurl
+        a = send_mail(subject='小智课堂注册确认', html_message=mailbody, from_email='aicollege@126.com', recipient_list=[email])
         print(a)
         print(mailbody)
 
