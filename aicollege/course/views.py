4b79ff06f9fe5946a8f58608f0b9f7174747c5c9
osierboy <2441531688@qq.com>
2018-08-22 23:39:22 +0800

    modify judgeCourse

--- aicollege/course/views.py
+++ aicollege/course/views.py
@@ -6,19 +6,6 @@ from django.db.models import Q
 from user.models import User
 from django.forms.models import model_to_dict
 
-def needmail(func):
-    def inner(*args, **kwargs):  # 1
-        req = args[0]
-        uid = req.session.get('uid')
-        if uid:
-            user = User.objects.filter(id__exact=uid)
-            user = user[0]
-            if not user.emailVerified:
-                return HttpResponse(json.dumps({"error": "邮箱未验证！"}))
-        return func(*args, **kwargs)  # 2
-    return inner
-
-
 ##进入初始界面的时候，根据用户ID和推送算法，加载课程信息
 # 根据需求返回课程信息
 
@@ -96,7 +83,6 @@ def getCourseInfo(request, page):
 
 
 # 返回用户购买课程列表，根据uid（所有课程的表）
-@needmail
 def getCourseInfoUid(request, page):
     print(request.COOKIES)
     try:
@@ -118,7 +104,6 @@ def getCourseInfoUid(request, page):
 #        raise Http404("课程小节加载失败")
 #    return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
 
-@needmail
 def judgeCourse(request, cid):
     print(request.COOKIES)
     f = False
@@ -172,7 +157,6 @@ def judgeCourse(request, cid):
     return JsonResponse(dict, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
-@needmail
 def addCourse(request, cid):
     uid = request.session['uid']
     user = User.objects.get(id=int(uid))
@@ -214,7 +198,6 @@ def keySearch(request):
 
 
 #获取一条评论,参数为字典类型，包含sid和str
-@needmail
 def addEvaluation(request):
     uid = request.session['uid']
     sid = request.GET['sid']  #篇id
@@ -240,7 +223,6 @@ def addEvaluation(request):
     return JsonResponse(dict, safe=False, json_dumps_params={'ensure_ascii': False})
 
 #返回一小节对应的所有评论
-@needmail
 def getEvaluation(request,sid):
     try:
         section = Section.objects.get(id = sid)
