dc5cbc9e5ec4edef2293af8193a4272550f103ba
Angelo Qin <qzhsjz@gmail.com>
2018-08-23 15:58:02 +0800

    fix message model

--- aicollege/message/models.py
+++ aicollege/message/models.py
@@ -6,8 +6,8 @@ from user.models import User
 
 class Message(models.Model):
     id = models.AutoField(primary_key=True)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name=sender)
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name=receiver)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
     time = models.DateTimeField(auto_now_add=True)
     subject = models.CharField(max_length=255)
     content = models.TextField()
