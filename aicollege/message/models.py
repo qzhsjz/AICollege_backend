30cc173efcf131b3592d0f2b92edb0e7f7b33874
Angelo Qin <qzhsjz@gmail.com>
2018-08-23 15:40:00 +0800

    add message system

--- aicollege/message/models.py
+++ aicollege/message/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+
+
+class Announcement(models.Model):
+    id = models.AutoField(primary_key=True)
+    time = models.DateTimeField(auto_now_add=True)
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
--- aicollege/message/views.py
+++ aicollege/message/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from .models import *
+from user.models import User
+from django.http import HttpResponse
+import json
+
 
 # Create your views here.
+
+
+def getmsg(request):
+    if request.method == 'GET':
+        uid = request.session.get('uid')
+        user = User.objects.get(id__exact=uid)
+        ancmt = Announcement.objects.filter(receiver__exact=user).all()
+        msg = Message.objects.filter(receiver__exact=user).all()
+        return HttpResponse(json.dumps({'Announcement': list(ancmt), 'Message': list(msg)}))
+    else:
+        return HttpResponse(json.dumps({"error": "请求不合法！"}))
+
+
+def sendmsg(request):
+    if request.method == 'POST':
+        try:
+            uid = request.session.get('uid')
+            user = User.objects.get(id__exact=uid)
+            msg = Message(sender=user)
+            recvuid = request.POST['receiver']
+            receiver = User.objects.get(id__exact=recvuid)
+            msg.receiver = receiver
+            msg.subject = request.POST['subject']
+            msg.content = request.POST['content']
+            msg.save()
+            return HttpResponse(json.dumps({"success": "OK"}))
+        except KeyError:
+            return HttpResponse(json.dumps({"error": "请求不合法！"}))
+    else:
+        return HttpResponse(json.dumps({"error": "请求不合法！"}))
