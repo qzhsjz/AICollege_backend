d9baecfc56af7ffccd14b442526d978a124dd76b
Angelo Qin <qzhsjz@gmail.com>
2018-09-04 23:17:02 +0800

    Localization

--- aicollege/aicollege/settings.py
+++ aicollege/aicollege/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
