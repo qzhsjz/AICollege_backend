529d744ec0ac9f7e84916b13f4ef357b1866a896
Angelo Qin <qzhsjz@gmail.com>
2018-09-05 08:11:42 +0800

    Localization

--- aicollege/message/views.py
+++ aicollege/message/views.py
@@ -8,8 +8,8 @@ import json
 
 # Create your views here.
 
-# TimeFormat = '%B是你的谎言,于%d日星期%w %X'
-TimeFormat = '%c'
+TimeFormat = '%B是你的谎言,于%d日%A %X'
+# TimeFormat = '%c'
 
 def getmsg(request):
     if request.method == 'GET':
