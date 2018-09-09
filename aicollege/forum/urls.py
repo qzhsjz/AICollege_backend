3c480eafc3f5198629aee66fb99423d8d5c292d5
Angelo Qin <qzhsjz@gmail.com>
2018-09-09 08:02:36 +0800

    add forum.

--- aicollege/forum/urls.py
+++ aicollege/forum/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.test),
-    #path('addEvaluation/<dict:dic>',views.addEvaluation),
-    #path('getevaluation/<int:sid>',views.getEvaluation),
+
 ]
